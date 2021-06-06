565d20b40fce2564bdeb40f2619a9d44e527f451
Dinesh Singh Gogini <goginidineshsingh0112@hotmail.com>
2021-06-06 16:20:21 +0530

    Naive Singleton implementation.

---
+++ Creational/Singleton/NaiveSingleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Naive implementation of Singleton pattern.
+    /// Simplest implementation of singleton pattern but does not really work well in multithreaded program.
+    /// </summary>
+    class NaiveSingleton
+    {
+        // Rule 1: Make constructor private.
+        private NaiveSingleton() { }
+
+        // Note: Not recommended for multi-threaded program.
+        private static NaiveSingleton _instance;
+
+        // This is the static method that controls the access to the singleton
+        // instance. On the first run, it creates a singleton object and places
+        // it into the static field. On subsequent runs, it returns the client
+        // existing object stored in the static field.
+        public static NaiveSingleton GetInstance()
+        {
+            if (_instance is null)
+            {
+                _instance = new NaiveSingleton();
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            NaiveSingleton naiveSingleton1 = NaiveSingleton.GetInstance();
+            NaiveSingleton naiveSingleton2 = NaiveSingleton.GetInstance();
+
+            if (naiveSingleton1.Equals(naiveSingleton2))
+            {
+                Console.WriteLine("Singleton implementation done.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
