951d4976bf0f8d2434120edf2af29be5f4f0f3af
Dinesh Singh Gogini <goginidineshsingh0112@hotmail.com>
2021-07-01 21:35:27 +0530

    Thread safe singleton pattern.

--- Creational/Singleton/Program.cs
+++ Creational/Singleton/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Singleton
 {
@@ -6,7 +7,44 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            NaiveSingletonClient();
+            ThreadSafeSingletonClient();
+            Console.ReadKey();
+        }
+
+        private static void ThreadSafeSingletonClient()
+        {
+            Console.WriteLine("Hello, This is thread safe singleton client!");
+
+            Console.WriteLine(
+               "{0}\n{1}\n\n{2}\n",
+               "If you see the same value, then singleton was reused (yay!)",
+               "If you see different values, then 2 singletons were created (booo!!)",
+               "RESULT:"
+            );
+
+            Thread thread1 = new Thread(() =>
+            {
+                var singleton = ThreadSafeSingleton.GetInstance("yay!");
+                Console.WriteLine(singleton.value);
+            });
+
+            Thread thread2 = new Thread(() =>
+            {
+                var singleton = ThreadSafeSingleton.GetInstance("booo!!");
+                Console.WriteLine(singleton.value);
+            });
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+        }
+
+        private static void NaiveSingletonClient()
+        {
+            Console.WriteLine("Hello, This is Naive Singleton Client!");
 
             NaiveSingleton naiveSingleton1 = NaiveSingleton.GetInstance();
             NaiveSingleton naiveSingleton2 = NaiveSingleton.GetInstance();
@@ -15,8 +53,6 @@ namespace Singleton
             {
                 Console.WriteLine("Singleton implementation done.");
             }
-
-            Console.ReadKey();
         }
     }
 }
---
+++ Creational/Singleton/ThreadSafeSingleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    // This Singleton implementation is called "double check lock". It is safe
+    // in multithreaded environment and provides lazy initialization for the
+    // Singleton object
+    public class ThreadSafeSingleton
+    {
+        private ThreadSafeSingleton() { }
+
+        // Some value for demonstration purpose.
+        public string value { get; set; }
+
+        private static ThreadSafeSingleton _instance;
+        private static readonly object _lock = new object();
+
+        public static ThreadSafeSingleton GetInstance(string value)
+        {
+            //  This conditional is needed to prevent threads stumbling over the
+            // lock once the instance is ready.
+            if (_instance is null)
+            {
+                // Now, imagine that the program has just been launched. Since
+                // there's no Singleton instance yet, multiple threads can
+                // simultaneously pass the previous conditional and reach this
+                // point almost at the same time. The first of them will acquire
+                // lock and will proceed further, while the rest will wait here.
+                lock (_lock)
+                {
+                    // The first thread to acquire the lock, reaches this
+                    // conditional, goes inside and creates the Singleton
+                    // instance. Once it leaves the lock block, a thread that
+                    // might have been waiting for the lock release may then
+                    // enter this section. But since the Singleton field is
+                    // already initialized, the thread won't create a new
+                    // object.
+                    if (_instance is null)
+                    {
+                        _instance = new ThreadSafeSingleton();
+                        _instance.value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
